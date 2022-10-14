c84df2fafc5468cd69aab6e81eaa21569093eeb1
isrbaral <3858343+isrbaral@users.noreply.github.com>
2022-10-14 12:16:44 -0500

    no pop up run as admin

--- fix-edgechrome.ps1
+++ fix-edgechrome.ps1
@@ -11,15 +11,17 @@ if (!(Test-Path $FolderName)) {
 # create ps file in tools folder
 $FileName = "$($FolderName)\set-ChromePolicies.ps1"
 if(!(Test-Path $FileName)) {
-    New-Item $FileName -ItemType File
-    
+    New-Item $FileName -ItemType File    
 }
-Clear-Content $FileName
+# Get SID for current user
+$sid = (([Security.Principal.WindowsIdentity]::GetCurrent()).User.Value)
 # in ps file add code for setting chrome and edge registry keys to reset newtab and homepage to default
-Add-Content $FileName "Set-ItemProperty -Path 'HKCU:\Software\Policies\Google\Chrome' -Name 'NewTabPageLocation' -Value ''
-`nSet-ItemProperty -Path 'HKCU:\Software\Policies\Google\Chrome' -Name 'HomepageLocation' -Value ''
-`nSet-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Edge' -Name 'NewTabPageLocation' -Value ''
-`nSet-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Edge' -Name 'HomepageLocation' -Value ''"
+Clear-Content $FileName
+Add-Content $FileName "if (!(test-path ""HKU:\"")) {New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS}
+Set-ItemProperty -Path 'HKU:\$sid\Software\Policies\Google\Chrome' -Name 'NewTabPageLocation' -Value ''
+Set-ItemProperty -Path 'HKU:\$sid\Software\Policies\Google\Chrome' -Name 'HomepageLocation' -Value ''
+Set-ItemProperty -Path 'HKU:\$sid\Software\Policies\Microsoft\Edge' -Name 'NewTabPageLocation' -Value ''
+Set-ItemProperty -Path 'HKU:\$sid\Software\Policies\Microsoft\Edge' -Name 'HomepageLocation' -Value ''"
 # $FileName = "$($FolderName)\runPS.vbs"
 # if(!(Test-Path $FileName)) {
 #     New-Item $FileName -ItemType File
@@ -30,11 +32,11 @@ Add-Content $FileName "Set-ItemProperty -Path 'HKCU:\Software\Policies\Google\Ch
 # Add-Content $FileName "Set WshShell = CreateObject(""WScript.Shell"")`nWshShell.Run ""powershell.exe -ExecutionPolicy Bypass -File $($FolderName)\set-ChromePolicies.ps1"", 0, False"
 
 # create scheduled task to run ps file every hour cmd /c start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File $($FolderName)\set-ChromePolicies.ps1
-$taskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -WindowStyle Hidden -noProfile -noInteractive -File $($FolderName)\set-ChromePolicies.ps1"
+$taskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File $($FolderName)\set-ChromePolicies.ps1"
 $taskTrigger = New-ScheduledTaskTrigger -AtLogOn
 $taskTrigger2 = New-ScheduledTaskTrigger -Daily -At 12am
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-$principal = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME" -LogonType "S4U" -RunLevel Highest
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 $task = Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger, $taskTrigger2 -Settings $settings -Principal $principal -Force -TaskName "ResetChromeEdgePolicies" -Description "Reset Chrome and Edge Policies"
 $task.Triggers[1].Repetition.Duration = "P1D"
 $task.Triggers[1].Repetition.Interval = "PT1H"
